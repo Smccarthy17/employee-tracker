f18401defa4cb268c2e3d079b45ed2069dcefc00
S. McCarthy <90977936+Smccarthy17@users.noreply.github.com>
2022-09-14 10:56:09 -0400

    add remove records, askid, new record, new roles questions

--- index.js
+++ index.js
@@ -206,5 +206,164 @@ const mainMenu = [
       ],
     };
 
-    
-    
+
+ const removeRecords = [
+    {
+      prefix: "",
+      type: "list",
+      name: "remove",
+      message: "What would you like to remove?",
+      choices: ["A department", "A role", "An employee", "Return to main menu"],
+      filter(val) {
+        let valParse = val.split(" ");
+        if (valParse[1] === "department") {
+          return "department";
+        }
+        if (valParse[1] === "role") {
+          return "role";
+        }
+        if (valParse[1] === "employee") {
+          return "employee";
+        } else {
+          return "exit";
+        }
+      },
+    },
+  ];
+  const askId = [
+    {
+      prefix: "",
+      type: "input",
+      name: "id",
+      message: `Please enter the id of the record you would like to remove `,
+    },
+  ];
+  const newRecord = {
+    recordId: [
+      {
+        prefix: "",
+        type: "input",
+        name: "id",
+        message: "Please enter the ID of the record you want to update:",
+      },
+    ],
+    department: [
+      {
+        prefix: "",
+        type: "input",
+        name: "newName",
+        message: "What should be the new name be?",
+      },
+    ],
+    role: [
+      {
+        prefix: "",
+        type: "list",
+        name: "role",
+        message: "What would like to update?",
+        choices: [
+          "A role's title",
+          "A role's salary",
+          "A role's department",
+          "Return to main menu",
+        ],
+        filter(val) {
+          let valParse = val.split(" ");
+          if (valParse[2] === "title") {
+            return "title";
+          }
+          if (valParse[2] === "salary") {
+            return "salary";
+          }
+          if (valParse[2] === "department") {
+            return "department";
+          }
+          if (valParse[2] === "main") {
+            return "main";
+          }
+        },
+      },
+      {
+        prefix: "",
+        type: "input",
+        name: "newTitle",
+        message: "What is the new name for this role?",
+        when(answers) {
+          if (answers.role === "title") {
+            return true;
+          }
+        },
+      },
+      {
+        prefix: "",
+        type: "input",
+        name: "newSalary",
+        message: "What is the new salary for this role?",
+        when(answers) {
+          if (answers.role === "salary") {
+            return true;
+          }
+        },
+      },
+      {
+        prefix: "",
+        type: "input",
+        name: "newDepartment",
+        message: "What is the new department for this role (Use ID)?",
+        when(answers) {
+          if (answers.role === "department") {
+            return true;
+          }
+        },
+      },
+    ],
+    employee: [
+      {
+        prefix: "",
+        type: "list",
+        name: "employee",
+        message: "What would like to update?",
+        choices: [
+          "An employee's role",
+          "An employee's manager",
+          "Return to main menu",
+        ],
+        filter(val) {
+          let valParse = val.split(" ");
+          if (valParse[2] === "manager") {
+            return "manager";
+          }
+          if (valParse[2] === "role") {
+            return "role";
+          }
+          if (valParse[2] === "main") {
+            return "main";
+          }
+        },
+      },
+      {
+        prefix: "",
+        type: "input",
+        name: "newRole",
+        message: "What is their new role (use ID)?",
+        when(answers) {
+          if (answers.employee === "role") {
+            return true;
+          }
+        },
+      },
+      {
+        prefix: "",
+        type: "input",
+        name: "newManager",
+        message: "Who is their new manager (use manager's employee ID)?",
+        when(answers) {
+          if (answers.employee === "manager") {
+            return true;
+          }
+        },
+      },
+    ],
+  };   
+
+  
